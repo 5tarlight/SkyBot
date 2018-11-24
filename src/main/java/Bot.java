d46107aec8f9cf5e236c655bf90dd178e0776657
GaruDeveloperStarlight <40519362+GaruDeveloperStarlight@users.noreply.github.com>
2018-11-24 22:16:00 +0900

    JDA

---
+++ src/main/java/Bot.java
@@ -0,0 +1,9 @@
+import lib.token.Token;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+public class Bot {
+    public static void main(String[] args) throws Exception{
+        JDA api = new JDABuilder(Token.token).build();
+    }
+}
